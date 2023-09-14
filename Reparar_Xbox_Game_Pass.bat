cff533001b25fc0f2c570046508ee4ac8697021a
OtaconEvil <otaconeviloficial@gmail.com>
2023-09-14 13:07:23 -0300

    Update Reparar_Xbox_Game_Pass.bat

--- Reparar_Xbox_Game_Pass.bat
+++ Reparar_Xbox_Game_Pass.bat
@@ -4,17 +4,22 @@ echo Reiniciando los servicios de Xbox Game Pass...
 echo.
 
 :: Detener los servicios de Xbox Game Pass
-sc stop XboxNetApiSvc
-sc stop XblAuthManager
-sc stop XblGameSave
+sc config XboxNetApiSvc start=disabled
+sc config XblAuthManager start=disabled
+sc config XblGameSave start=disabled
 
 :: Espera unos segundos para asegurar que los servicios se detengan completamente
 timeout /t 5 /nobreak > nul
 
 :: Iniciar los servicios de Xbox Game Pass
-sc start XboxNetApiSvc
-sc start XblAuthManager
-sc start XblGameSave
+sc config XboxNetApiSvc start=auto
+sc config XblAuthManager start=auto
+sc config XblGameSave start=auto
+
+:: Inicia los servicios
+net start XboxNetApiSvc
+net start XblAuthManager
+net start XblGameSave
 
 echo.
 echo Todos los servicios de Xbox Game Pass han sido reiniciados.
